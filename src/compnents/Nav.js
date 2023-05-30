3cc7dc59917eee9309d9227fec43587d1b799180
pjthakur <priyanshjadon@gmail.com>
2023-05-30 16:53:53 +0530

    added assets

---
+++ src/compnents/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
